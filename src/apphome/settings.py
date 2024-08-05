0893c704f71547c5eb47459c213e9e95a7807a2b
Simon Carr <simonjcarr@gmail.com>
2024-08-05 12:17:56 +0100

    prepare for railway

--- src/apphome/settings.py
+++ src/apphome/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure-33*@bh18-$z$%o+k+i2z)i%gixj-3cb15zcjxh1yde7wj***r_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app",
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
--- src/apphome/views.py
+++ src/apphome/views.py
@@ -5,39 +5,26 @@ from visits.models import PageVisit
 
 this_dir = pathlib.Path(__file__).resolve().parent
 
-def home_page_view(request, *args, **kwargs):
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+
+
+def about_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = round(page_qs.count() / qs.count() * 100.0,1)
+    except ZeroDivisionError:
+        percent = 0
     my_title = "My Page"
     my_context = {
         "page_title": my_title,
         "page_visit_count": page_qs.count(),
-        "percent": round(page_qs.count() / qs.count() * 100.0,1),
+        "percent": percent,
         "total_visit_count": qs.count(),
     }
     path = request.path
     html_template = "home.html"
     PageVisit.objects.create(path=request.path)
     return render(request, html_template, my_context)
-
-
-
-def my_old_home_page_view(request, *args, **kwargs):
-    my_title = "My Page"
-    my_context = {"page_title": my_title}
-    html_= """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{page_title}</title>
-</head>
-<body>
-    <h1>This is something</h1>
-</body>
-</html>
-""".format(**my_context)
-    # html_file_path = this_dir / "home.html"
-    # html_ = html_file_path.read_text()
-    return HttpResponse(html_)
