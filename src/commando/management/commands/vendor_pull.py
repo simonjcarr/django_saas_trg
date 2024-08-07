05eb0f31d337eecf600960def98773f9fc897a68
Simon Carr <simonjcarr@gmail.com>
2024-08-07 10:32:51 +0100

    download static vendor files

---
+++ src/commando/management/commands/vendor_pull.py
@@ -0,0 +1,31 @@
+import helpers
+
+from typing import Any
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+STATICFILES_VENDOR_DIR = getattr(settings, "STATICFILES_VENDOR_DIR")
+
+VENDOR_STATICFILES = {
+    "flowbite.min.css": "https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.css",
+    "flowbite.min.js": "https://cdn.jsdelivr.net/npm/flowbite@2.5.1/dist/flowbite.min.js",
+}
+
+class Command(BaseCommand):
+
+    def handle(self, *args:any, **kwargs:any):
+        self.stdout.write("Downloading vendor static files")
+        completed_urls = []
+        for name, url in VENDOR_STATICFILES.items():
+            out_path = STATICFILES_VENDOR_DIR / name
+            dl_success = helpers.download_to_local(url, out_path)
+            if dl_success:
+                completed_urls.append(url)
+            else:
+                self.stdout.write(
+                    self.style.ERROR(f"Failed to download {url} to {out_path}")
+                )
+        if set(completed_urls) == set(VENDOR_STATICFILES.values()):
+            self.stdout.write(self.style.SUCCESS("Successfully updated vendor static files"))
+        else:
+            self.stdout.write(self.style.ERROR("Failed to update vendor static files"))
